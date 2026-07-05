ebcd07ddfc02097901cc77215534271311c85914
ccyybn <ccyybn@gmail.com>
2026-07-05 17:26:31 +0800

    project 4 completed

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,53 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@color
+M=0
+
+(LOOP)
+@24576
+D=M
+@PRINT
+D;JEQ
+
+@color
+M=-1
+
+(PRINT)
+
+@8192
+D=A
+@n
+M=D
+
+@16383
+D=A
+@offset
+M=D
+
+(PRINT_LOOP)
+@offset
+D=M
+@n
+D=D+M
+@addr
+M=D
+
+@color
+D=M
+
+@addr
+A=M
+M=D
+
+@n
+M=M-1
+D=M
+
+@PRINT_LOOP
+D;JGT
+
+@color
+M=0
+@LOOP
+0;JMP
